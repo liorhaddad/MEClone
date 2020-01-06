8214cef28aabcd38c3f40ec4b9ee2629bcc6ca11
liorhaddad <liorhaddad1234@gmail.com>
2020-01-06 15:43:51 +0200

    Installer now runs independantly
    Installer no longer uses sitems to install

--- sinstall.lua
+++ sinstall.lua
@@ -1,2 +1,23 @@
+function gitget(filelocation)
+	local site = http.get("https://raw.githubusercontent.com/liorhaddad/MEClone/master/" .. filelocation)
+	if not (site == nil) then
+		local sitetext = site.readAll()
+		site.close()
+		return sitetext
+	else
+		return
+	end
+end
+
+function gitsave(gitlocation,filelocation)
+	local site = http.get("https://raw.githubusercontent.com/liorhaddad/MEClone/master/" .. gitlocation)
+	local sitetext = site.readAll()
+	site.close()
+	local file = fs.open(filelocation,"w")
+	file.flush()
+	file.write(sitetext)
+	file.close()
+	return sitetext
+end
 print("Server updating test")
-sitems.gitsave("server/.version","/disk/.version")
+gitsave("server/.version","/disk/.version")
